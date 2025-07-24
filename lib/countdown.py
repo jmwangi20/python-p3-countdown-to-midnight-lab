1d2d7d5c2eabeb133e5611545c6cfeddf88bf410
John Kihugo Mwangi <johnmwangi13119@gmail.com>
2025-07-24 13:16:44 +0300

    Countdown

--- lib/countdown.py
+++ lib/countdown.py
@@ -1,1 +1,15 @@
 # your code goes here!
+
+import time 
+
+def countdown(num):
+    for i in range(num,0,-1):
+        print(f'{i}SECOND(S)!')
+
+print("HAPPY NEW YEAR!")
+
+def countdown_with_sleep(num):
+    for i in range(num,0,-1):
+        print(f'{i}SECOND(S)!')
+        time.sleep(2)
+    
